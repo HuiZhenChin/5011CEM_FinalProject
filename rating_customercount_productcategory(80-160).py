03c7d5aa2dcfa87c5b9d94d52c14e644533d13ed
HuiZhenChin <132343277+HuiZhenChin@users.noreply.github.com>
2023-11-22 22:57:56 +0800

    Add files via upload

--- rating_customercount_productcategory(80-160).py
+++ rating_customercount_productcategory(80-160).py
@@ -65,76 +65,77 @@ category_summary_df = category_summary_df.sort_values(by='Count', ascending=Fals
 # select the top N product categories to include in the graph (20 product categories only based on the table shown)
 top_categories = 72
 
-# Select the top N product categories to include in the graph
+# slect the top N product categories to include in the graph
 top_category_names = category_summary_df.iloc[:top_categories, 0].tolist()
 
-# Create a numerical mapping for product category names
+# create a numerical mapping for product category names
 category_mapping = {category: i for i, category in enumerate(top_category_names)}
 
-# Filter the data for the selected top product categories and the selected delivery time range
+# filter the data for the selected top product categories and the selected delivery time range
 filtered_data = merged_data[
     (merged_data['product_category_name_english'].isin(top_category_names)) &
     (merged_data['day_difference'] >= 80) &
     (merged_data['day_difference'] <= 160)
 ]
 
-# Create a column for review count
+# create a column for review count
 filtered_data['review_count'] = 1
 
-# Remove duplicate entries
+# remove duplicate entries
 filtered_data_no_duplicates = filtered_data.drop_duplicates(subset=['order_id', 'customer_id'])
 
-# Create a pivot table to show the customer count for each rating of each product category
+# create a pivot table to show the customer count for each rating of each product category
 pivot_table_customer_count = filtered_data_no_duplicates.groupby(['product_category_name_english', 'review_score'])[
     'review_count'].count().unstack(fill_value=0)
 
-# Use the values from the table for plotting
+# use the values from the table for plotting
 customer_count = pivot_table_customer_count.values
 
-# Get the values of product category and ratings
+# get the values of product category and ratings
 product_categories = [str(category) for category in pivot_table_customer_count.index]
 ratings = [str(rating) for rating in pivot_table_customer_count.columns]
 
-# Create meshgrid for 3D bar graph
+# create meshgrid for 3D bar graph
 x, y = np.meshgrid(np.arange(len(ratings)), np.arange(len(product_categories)))
 z = customer_count[pivot_table_customer_count.index.isin(product_categories), :]
 
-# Bar width
+# bar width
 dx = dy = 0.8
 
-# Graph size
+# graph size
 fig = plt.figure(figsize=(32, 37))
 ax = fig.add_subplot(111, projection='3d')
 
-# Assign colors to each product category
+# assign colors to each product category
 norm = Normalize(vmin=0, vmax=len(product_categories))
 colors = cm.GnBu(norm(np.arange(len(product_categories))))
 
-# Reshape colors to match the shape of z
+# reshape colors to match the shape of z
 colors_reshaped = colors.reshape(1, -1, 4)
 
-# Repeat the color array for each bar in the plot
+# repeat the color array for each bar in the plot
 colors_repeated = np.repeat(colors_reshaped, len(ratings), axis=1)
 
-# Plot the 3D bar graph
-# Swap x and y in the bar3d function
+# plot the 3D bar graph
 bars = ax.bar3d(y.ravel(), x.ravel(), np.zeros_like(z).ravel(), dy, dx, z.ravel(), shade=True,
                 color=colors_repeated[0])
 
-# Update y-axis tick labels and labels
+
+# y axis label
 ax.set_yticks(np.arange(len(ratings)))
 ax.set_yticklabels(ratings)
 ax.set_ylabel('Rating')
 
-# Update x-axis tick labels and labels
+# x axis label
 ax.set_xticks(np.arange(len(product_categories)))
-ax.set_xticklabels(product_categories)
+ax.set_xticklabels(np.arange(0, 22, 1))
 ax.set_xlabel('Product Category')
 
 # set axis title
 ax.set_zlabel('Customer Count')
 ax.set_title('3D Bar Graph of Customer Count by Rating and Product Category (Delivery Time: 80-160 days)', fontsize=28)
 
+# set up legend
 legend_labels_with_numbers = [f"{i}: {category}" for i, category in enumerate(product_categories)]
 legend_handles = [plt.Rectangle((0, 0), 1, 1, color=cm.Blues(norm(i))) for i in range(len(product_categories))]
 ax.legend(legend_handles, legend_labels_with_numbers, loc='upper left', bbox_to_anchor=(1.05, 0.5, 0.1, 0.5),
@@ -142,9 +143,9 @@ ax.legend(legend_handles, legend_labels_with_numbers, loc='upper left', bbox_to_
 
 plt.show()
 
-# Calculate total customer count for each category
+# calculate total customer count for each category
 total_customer_count = pivot_table_customer_count.loc[product_categories].sum(axis=1)
 
-# Display the total customer count as a pivot table
+# display the total customer count as a pivot table
 print("\nTotal Customer Count by Product Category (Pivot Table):")
 print(total_customer_count.reset_index(name='Total Customer Count'))
